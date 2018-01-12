857a7a269c54a3295bc35a1b1e2c5b784d0676b5
Charlie <jcdelarosa@verizon.net>
2018-01-12 00:55:39 -0600

    Begin Deck of Cards assignment.

---
+++ deck_of_cards/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace deck_of_cards
+{
+    public class Card {
+        string stringVal;
+        string suit;
+        int val = 0;
+    }
+
+    public class Deck {
+        List<Card> cards = new List<Card>();
+
+        public Deck() {
+            for (int i = 0; i < 4; i++) {
+                // Left off here 1/12/18 @ 12:54;
+            }
+        }
+    }
+
+    public class Player {
+        string name;
+        List<Card> hand = new List<Card>();
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
--- human/Program.cs
+++ human/Program.cs
@@ -2,11 +2,30 @@
 
 namespace human
 {
+    public class Human {
+        public string name;
+        public int strength = 3;
+        public int intelligence = 3;
+        public int dexterity = 3;
+        public int health;
+
+        public Human(string name) {
+
+        }
+
+        public Human(string name, int strength, int intelligence, int dexterity, int health) {
+
+        }
+
+        public void Attack(Human victim) {
+            victim.strength = 5 * this.strength;
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            
         }
     }
 }
