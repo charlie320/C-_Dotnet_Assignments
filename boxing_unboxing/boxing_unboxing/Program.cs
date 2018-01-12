d6aa961614fc238c2efef5b06e2d61bca10853fb
Charlie <jcdelarosa@verizon.net>
2018-01-11 23:06:42 -0600

    boxing_unboxing assignment

---
+++ boxing_unboxing/boxing_unboxing/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace boxing_unboxing
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int sum = 0;
+            List<object> list_of_objects = new List<object>();
+            list_of_objects.Add(7);
+            list_of_objects.Add(28);
+            list_of_objects.Add(-1);
+            list_of_objects.Add(true);
+            list_of_objects.Add("chair");
+
+            foreach (object list_item in list_of_objects) {
+                Console.WriteLine(list_item);
+            }
+
+            foreach (object list_item in list_of_objects)
+            {
+                if (list_item is int) {
+                    sum += (int)list_item;
+                }
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
