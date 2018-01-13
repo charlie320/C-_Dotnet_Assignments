6ec270fb729bd47044427a1061d8936aa44eee2f
Charlie <jcdelarosa@verizon.net>
2018-01-13 14:28:53 -0600

    simple_crud update

---
+++ music-linq/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using JsonData;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //Collections to work with
+            List<Artist> Artists = JsonToFile<Artist>.ReadJson();
+            List<Group> Groups = JsonToFile<Group>.ReadJson();
+
+            //========================================================
+            //Solve all of the prompts below using various LINQ queries
+            //========================================================
+
+            // foreach (Artist artist in Artists) {
+            //     Console.WriteLine(artist.Hometown);
+            // }
+
+            //There is only one artist in this collection from Mount Vernon, what is their name and age?
+            IEnumerable<Artist> ArtistList = Artists.Where(artist => artist.Hometown == "Mount Vernon");
+
+            Console.Write("Artists from Mount Vernon include ");
+            foreach (Artist ind in ArtistList) {
+                Console.WriteLine("{0} who is {1} years old.", ind.RealName, ind.Age);
+            }
+
+            //Who is the youngest artist in our collection of artists?
+            int youngestAge = Artists.Select(artist => artist.Age).Min();
+            IEnumerable<Artist> ArtistListAge = Artists.Where(artist => artist.Age == youngestAge);
+            foreach (Artist young in ArtistListAge) {
+            Console.WriteLine("The artist with the youngest age is {0}", young.RealName);
+            }
+            // another way to retrieve the youngest artists
+            IEnumerable<Artist> ArtistAgeSort = Artists.OrderBy(artist => artist.Age);
+            Console.WriteLine("The artist with the youngest age is {0}", ArtistAgeSort.First().RealName);
+
+            //Display all artists with 'William' somewhere in their real name
+            IEnumerable<Artist> WilliamName = Artists.Where(artist => artist.RealName.Contains("William"));
+            foreach (Artist william in WilliamName) {
+                Console.WriteLine("An artist with {0} in his/her name: {1}.", "\"William\"", william.RealName);
+            }
+            //Display the 3 oldest artist from Atlanta
+            IEnumerable<Artist> ArtistAgeSortDesc = Artists.Where(artist => artist.Hometown == "Atlanta").OrderByDescending(artist => artist.Age).Take(3);
+            Console.WriteLine("The 3 oldest artists from Atlanta are:");
+            foreach (Artist elder in ArtistAgeSortDesc) {
+                Console.WriteLine("{0}", elder.RealName );
+            }
+            //(Optional) Display the Group Name of all groups that have members that are not from New York City
+
+            //(Optional) Display the artist names of all members of the group 'Wu-Tang Clan'
+        }
+    }
+}
---
+++ simple_crud_mysql/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using DbConnection;
+
+namespace simple_crud_mysql
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            // List<Dictionary<string, object>> myQuery = DbConnector.Query("SELECT first_name, favorite_number FROM users");
+            // foreach (Dictionary<string,object> dict in myQuery) {
+            //     foreach (KeyValuePair<string,object> entry in dict) {
+            //         Console.WriteLine(entry.Key + " " + entry.Value);
+            //     }
+            // }
+
+            Console.WriteLine("Please provide some input.");
+            string InputLine = Console.ReadLine();
+            Console.WriteLine("Here is the input you provided:  {0}", InputLine);
+        }
+    }
+}
