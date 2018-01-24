7e4f7ebf9dd999090e7c1b8bbf99521cc93bb414
Charlie <jcdelarosa@verizon.net>
2018-01-24 01:50:21 -0600

    Worked on Login and Registration page.

--- TheWall/Controllers/HomeController.cs
+++ TheWall/Controllers/HomeController.cs
@@ -3,17 +3,59 @@ using System.Collections.Generic;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Http;
+using DbConnection;
+using TheWall.Models;
 
 namespace TheWall.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: /Home/
+
+        private readonly DbConnector _dbConnector;
+
+        public HomeController(DbConnector connect) {
+            _dbConnector = connect;
+        }      
+
         [HttpGet]
         [Route("")]
+        [Route("index")]
         public IActionResult Index()
         {
-            return View();
+            // List<Dictionary<string, object>> AllUsers = _dbConnector.Query("SELECT * FROM users");
+
+            return View("Index");
+        }
+
+        [HttpGet]
+        [Route("registerform")]
+        public IActionResult RegisterForm() {
+            return PartialView("~/Views/Shared/_Registration.cshtml");
+        }
+
+        [HttpPost]
+        [Route("")]
+        [Route("index")]
+        public IActionResult Index(User user) {
+            Console.WriteLine(user.FirstName);
+            if (!ModelState.IsValid) {
+                ViewBag.IsValid = false;
+            }
+            return View("_Registration", user);
+        }
+
+        [HttpGet]
+        [Route("loginform")]
+        public IActionResult LoginForm() {
+            return PartialView("~/Views/Shared/_Login.cshtml");
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public IActionResult Login(User user) {
+
+            ViewBag.error = "Email address and/or password are invalid.";
+            return View("_Login");
         }
     }
 }
---
+++ TheWall/Models/BaseEntity.cs
@@ -0,0 +1,7 @@
+namespace TheWall.Models
+
+{
+    public abstract class BaseEntity {
+        
+    }
+}
